9750a53b8abe273284c8c3bf471f213cfa542b97
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2019-11-17 15:22:14 -0300

    Primeira Migration

---
+++ Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Duracao { get; set; }
+        public int Ano { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Locadora.Models;
 
 namespace Locadora
 {
@@ -33,6 +35,10 @@ namespace Locadora
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<LocadoraContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("LocadoraContext"), builder =>
+                        builder.MigrationsAssembly("Locadora")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
